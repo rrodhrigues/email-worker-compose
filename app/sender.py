4aa9a49a35f5311b36abe04207a2c64557e5a904
Renato <rrodriguez@outlook.com.br>
2020-02-24 11:09:07 -0300

    refact: renomenado o nome do metodo

--- app/sender.py
+++ app/sender.py
@@ -1,7 +1,7 @@
 from  bottle import route, run, request
 
 @route('/', method='POST')
-def sen(): 
+def send(): 
     assunto = request.forms.get('assunto')
     mensagem = request.forms.get('mensagem')
 
